2d8e5ff9bdd3bbfdcbb70c39b3b2b76ccdd00969
Kaushikkar <kaushikkar2002@gmail.com>
2023-04-30 21:37:52 +0530

    Enemy_Detection FIXES
    MAKE A WORKING ENEMY DETECTION SYSTEM

--- Project2D23D/Assets/Scripts/Enemy_detection.cs
+++ Project2D23D/Assets/Scripts/Enemy_detection.cs
@@ -7,10 +7,17 @@ public class Enemy_detection : MonoBehaviour
     public bool health_reduce=false;
     void OnCollisionEnter(Collision col)
     {
+        
         if(col.gameObject.tag=="Enemy")
             health_reduce=true;
-        else
-            health_reduce=false;
-        Debug.Log(health_reduce);
+        //Debug.Log(health_reduce);
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if(col.gameObject.tag=="Enemy")
+        {
+            health_reduce =false;
+        }
+        //Debug.Log(health_reduce);
     }
 }
--- Project2D23D/Assets/Scripts/Player_health.cs
+++ Project2D23D/Assets/Scripts/Player_health.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class Player_health : MonoBehaviour
 {
     Enemy_detection enemy_Detection;
-    float max_health=5.0f;
-
-    // Update is called once per frame
+    float max_health=5;
+    float current_health;
+    bool damage=false;
+    bool damage_taken=false;
+    void Start()
+    {
+        current_health=max_health;
+    }
     void Update()
     {
-        Debug.Log(gameObject.GetComponent<Enemy_detection>().health_reduce);
-        if(gameObject.GetComponent<Enemy_detection>().health_reduce)
+        Debug.Log("Current health"+current_health);
+        damagetaken(damage);
+    }
+    void damagetaken(bool damage)
+    {
+        damage=gameObject.GetComponent<Enemy_detection>().health_reduce;
+        if(damage==true && !damage_taken)
         {
-            Debug.Log("Done");
+            current_health-=1;
+            damage_taken=true;
+        }
+        else if(!damage)
+        {
+            damage_taken=false;
+        }
+         if(current_health==0)
+        {
+            Debug.Log("DEAD");
         }
     }
 }
